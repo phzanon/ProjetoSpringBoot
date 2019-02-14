41df8982704cb4c7beceede684d0eb6de575cea8
phzanon <p.hzanon123@gmail.com>
2019-02-14 18:18:24 -0200

    Deploy

---
+++ src/main/java/com/ProjetSpringBoot/repository/EventoRepository.java
@@ -0,0 +1,9 @@
+package com.ProjetSpringBoot.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ProjetSpringBoot.models.Evento;
+
+public interface EventoRepository extends CrudRepository<Evento, String>{
+
+}
