950833737e8f72f8fcc1f379dbf1bf5f9d3acf25
phzanon <p.hzanon123@gmail.com>
2019-02-15 14:25:58 -0200

    validação dos formulários e inserção de mensagem de erro

--- src/main/java/com/ProjetSpringBoot/models/Convidado.java
+++ src/main/java/com/ProjetSpringBoot/models/Convidado.java
@@ -3,12 +3,15 @@ package com.ProjetSpringBoot.models;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 public class Convidado {
 
 	@Id
+	@NotEmpty
 	private String rg;
+	@NotEmpty
 	private String nomeConvidado;
 	
 	@ManyToOne
